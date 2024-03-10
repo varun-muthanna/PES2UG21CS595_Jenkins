6efdaeac8a5eb8d64c3019c907dda4a7e5aa7088
varun-muthanna <97382175+varun-muthanna@users.noreply.github.com>
2024-03-10 15:52:12 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World , I'm Varun Muthanna K A \n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout<<"Webhhooks successfully enabled";
+  cout<<"Webhhooks successfully enabled"<<endl;
   return 0;
 }
